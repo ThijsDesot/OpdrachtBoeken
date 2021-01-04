a16aa129e71c81bfc1ac4ded540d020e27718c1e
ThijsDesot <thijs.desot@gmail.com>
2021-01-04 10:16:10 +0100

    Add project files.

---
+++ OpdrachtBoeken/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OpdrachtBoeken
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
